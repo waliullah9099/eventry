f543ec10bd5a6c8c3792e839225d9f23ef1ba233
MD Waliullah <waliullah9099@gmail.com>
2024-12-26 10:21:16 +0600

    create login and register page

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,4 +1,3 @@
-
 import Image from "next/image";
 import Link from "next/link";
 
@@ -8,17 +7,16 @@ const Navbar = () => {
       <div className="container flex justify-between items-center py-4">
         <div className="nav-brand">
           <Link href="/">
-            <Image
-              src="/logo.svg"
-              alt="Eventry"
-              width={135}
-              height={135} />
+            <Image src="/logo.svg" alt="Eventry" width={135} height={135} />
           </Link>
         </div>
 
         <ul className="flex gap-4 text-[#9C9C9C]">
           <li>About</li>
           <li>Contact Us</li>
+          <Link href={"/login"}>
+            <li>Login</li>
+          </Link>
         </ul>
       </div>
     </nav>
