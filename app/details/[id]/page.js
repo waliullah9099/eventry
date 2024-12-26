54f60e4f45bd3109f048b594a5430a9f457378d7
MD Waliullah <waliullah9099@gmail.com>
2024-12-26 20:54:51 +0600

    handle dynamic meta data

--- app/details/[id]/page.js
+++ app/details/[id]/page.js
@@ -3,6 +3,14 @@ import EventVenue from "@/components/details/EventVenue";
 import HeroSection from "@/components/details/HeroSection";
 import { getEventById } from "@/db/quires";
 
+async function generateMetadata({ params: { id } }) {
+  const event = await getEventById(id);
+  return {
+    title: `Eventry - ${event?.name}`,
+    description: event?.description,
+  };
+}
+
 const EventDetailsPage = async ({ params: { id } }) => {
   const eventInfo = await getEventById(id);
   console.log(eventInfo);
